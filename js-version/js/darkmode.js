d65bef943631cdd3cc02fc807a6cf0c0566a9dc5
“manar <albreikimanar@gmail.com>
2026-04-13 12:37:32 +0400

    edit js/darkmode.js

--- js-version/js/darkmode.js
+++ js-version/js/darkmode.js
@@ -3,10 +3,10 @@ document.addEventListener("DOMContentLoaded", function(){
 
 let toggle = document.getElementById("darkToggle");
 
-// 1. افتراضي = لايت (إذا ما فيه شيء محفوظ)
+//  افتراضي = لايت (إذا ما فيه شيء محفوظ)
 let savedMode = localStorage.getItem("mode");
 
-// 2. تطبيق الوضع المحفوظ فقط
+//  تطبيق الوضع المحفوظ فقط
 if(savedMode === "dark"){
 document.body.classList.add("dark");
 if(toggle) toggle.checked = true;
@@ -15,7 +15,7 @@ document.body.classList.remove("dark");
 if(toggle) toggle.checked = false;
 }
 
-// 3. عند تغيير الزر
+//  عند تغيير الزر
 if(toggle){
 toggle.addEventListener("change", function(){
 
